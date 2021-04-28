54c8e8a226d6b12957cc6bd5f5cbe72928ea27b5
diablo3775 <anilkudupudi@gmail.com>
2021-04-28 13:22:41 +0530

    second commit

---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Story from './Story';
+import './StoryReel.css'
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+      <Story
+        image='https://i.pinimg.com/originals/65/05/e0/6505e03880b21909606e203f88011222.jpg'
+        profileSrc="https://scontent.frja1-1.fna.fbcdn.net/v/t31.18172-8/27993371_828308434040169_4106439154212818397_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=DDfq4NkEO2UAX_wNnSF&_nc_ht=scontent.frja1-1.fna&oh=bc2d36d3357f1a1642b9930bc660b6fe&oe=60A6C463"
+        title="Harish"
+      />
+      <Story
+        image='https://i1.wp.com/super.black/wp-content/uploads/2020/08/black-manta-fortnite.jpg?resize=875%2C915&ssl=1'
+        profileSrc="https://scontent.frja1-1.fna.fbcdn.net/v/t1.6435-9/118010260_2630789207139282_4296144522186771997_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=67mkRU2hDyAAX8dBQ_7&_nc_ht=scontent.frja1-1.fna&oh=8346f6cf54c49f8d484dd121f9a0cecb&oe=60A82601"
+        title="Diablo"
+      />
+      <Story
+        image='https://i.pinimg.com/originals/c0/a3/8c/c0a38cab4243caf9f5c61790a7eb6bf9.jpg'
+        profileSrc="https://scontent.frja1-1.fna.fbcdn.net/v/t1.18169-9/10373970_774927455951117_6335097740009410246_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=GApsDbWgE38AX9ysemn&_nc_ht=scontent.frja1-1.fna&oh=b06335d51b5e774bb8aeabe1683e5df8&oe=60A6AC71"
+        title="Rebel"
+      />
+      <Story
+        image='https://progameguides.com/wp-content/uploads/2019/04/fortnite-outfit-stealth-reflex.jpg'
+        profileSrc="https://scontent.frja1-1.fna.fbcdn.net/v/t1.6435-9/116623710_1128955614171682_5826165346466827905_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=svR1wjofREAAX8emDET&_nc_ht=scontent.frja1-1.fna&oh=d10209646cc569df7251d13ab6b52500&oe=60A7E0C8"
+        title="Nanu"
+      />
+      <Story
+        image='https://i2.wp.com/fortniteskins.net/wp-content/uploads/2018/04/tomatohead-skin-02.png?quality=90&strip=all&ssl=1'
+        profileSrc="https://scontent.frja1-1.fna.fbcdn.net/v/t1.6435-9/131567039_2823481621261112_7974154119226104395_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=FJ6T5CpSwpkAX-4hI5Y&_nc_ht=scontent.frja1-1.fna&oh=66090e694339a726c9dc3d24ebdc1980&oe=60A8F5F9"
+        title="Subhash"
+      />
+        </div>
+    )
+}
+
+export default StoryReel; 
