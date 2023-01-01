438136ce9384470b2f5ea264fb0ce3517138ff9b
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-01-01 10:13:38 +0530

    Update Widgets.js

--- src/components/Widget/Widgets.js
+++ src/components/Widget/Widgets.js
@@ -5,9 +5,8 @@ function Widgets() {
   return (
     <div className="widgets">
       <iframe
-        // src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCodingTreeFoundation&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCodingTreeFoundation&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         width="340"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
         height="100%"
         style={{ border: 'none', overflow: 'hidden' }}
         scrolling="no"
